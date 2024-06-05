73c2836e96747299323474d3de4eb0944f6e42aa
Matias-F-Diaz-C <mfc.diaz@alumno.etec.um.edu.ar>
2024-06-05 12:41:49 -0300

    Saludo nombre y apellido

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ def hello_world():
 def bye_world():
     return "<p>Bye, World!</p>"
 
-@app.route("/saludo/nombre")
+@app.route("/saludo/<nombre>")
 def saludar(nombre):
     return f"Hola {nombre}"
+
+@app.route("/nomape/<nombre>/<apellido>")
+def nomape(nombre, apellido):
+    return f"Hola {nombre} {apellido}"
 
 app.run()
